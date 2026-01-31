9feeb3610c76cd5223e05e839d6c057acef0a427
Rudrank Riyam <rudrankriyam@gmail.com>
2026-01-31 19:33:15 +0530

    fix: drop unused beta notifications import

--- internal/cli/testflight/beta_notifications.go
+++ internal/cli/testflight/beta_notifications.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
-
-	"github.com/rudrankriyam/App-Store-Connect-CLI/internal/asc"
 )
 
 // BetaNotificationsCommand returns the beta notifications command group.
